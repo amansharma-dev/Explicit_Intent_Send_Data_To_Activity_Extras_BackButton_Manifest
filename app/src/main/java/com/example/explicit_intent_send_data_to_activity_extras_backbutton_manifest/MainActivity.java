00c9530e6959dfbec929874ffa7fc87114f19ff7
Aman Sharma <amansharma.11@hotmail.com>
2019-07-12 16:21:09 +0530

    Initial commit

---
+++ app/src/main/java/com/example/explicit_intent_send_data_to_activity_extras_backbutton_manifest/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.explicit_intent_send_data_to_activity_extras_backbutton_manifest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
